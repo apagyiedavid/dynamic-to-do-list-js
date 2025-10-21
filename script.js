3bd77fb654f1c46b77abca7bd830d6ba37cc7458
apagyiedavid <apagyiek@gmail.com>
2025-10-21 04:26:48 +0000

    commit again

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
  // Wait for the DOM to be fully loaded before executing the script
         document.addEventListener('DOMContentLoaded', function() {
             // Select DOM elements
-            const addButton = document.getElementById('add-button');
+            const addButton = document.getElementById('add-task-btn');
             const taskInput = document.getElementById('task-input');
             const taskList = document.getElementById('task-list');
             const totalTasksElement = document.getElementById('total-tasks');
